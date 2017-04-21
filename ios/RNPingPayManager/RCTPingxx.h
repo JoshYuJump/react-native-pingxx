dbf730235e39a03870bcbffcf368791452df5e37
Josh Yu <josh.yu_8@live.com>
2017-04-21 16:08:55 +0800

    Support For React Native 0.40.0

--- ios/RNPingPayManager/RCTPingxx.h
+++ ios/RNPingPayManager/RCTPingxx.h
@@ -6,7 +6,12 @@
 //  Copyright © 2015 erica. All rights reserved.
 //
 #import <Foundation/Foundation.h>
+
+#if __has_include("Base/RCTBridgeModule.h")
 #import "Base/RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif  
 
 @interface RCTPingxx : NSObject<RCTBridgeModule>
 
